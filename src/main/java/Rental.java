96948b22da842093c291ff876845bfc485710ab4
Myeongseok Baek <msbaek@daumcorp.com>
2011-12-13 16:24:11 +0900

    add sample files

---
+++ src/main/java/Rental.java
@@ -0,0 +1,27 @@
+//From book: 'Refactoring' by Martin Fowler
+//This is the original code before refactoring begins
+
+/**
+ * The rental class represents a customer renting a movie.
+ */
+public class Rental {
+
+	private Movie _movie;
+	private int _daysRented;
+	
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+	
+	
+	
+}
---
+++ src/test/java/VideoStoreTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class VideoStoreTest {
+    private Customer customer;
+
+    @Before
+    public void setUp() {
+        customer = new Customer("Fred");
+    }
+
+    @Test
+    public void testSingleNewReleaseStatement() {
+//        customer.addRental()
+    }
+}
