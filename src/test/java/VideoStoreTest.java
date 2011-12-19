cfba5875212babd5586d7d97e89dbdfdbd625468
Myeongseok Baek <msbaek@daumcorp.com>
2011-12-19 17:25:49 +0900

    enhance tests for comparing values instead of UI
    포맷팅된 문자열이 아니라 값을 변경하도록 변경
    
    마지막 테스트는 스트링을 검사하도록 유지하는 것이 좋을 것 같다.
    하지만 스트링 검사와 값을 검사하는 것이 섞여 있는 것은 원치않는다.
    
    기존 테스트를 복사해서 testMultipleRegularStatementFormat을 추가
    기존 테스트는 testMultipleRegularStatementTotals로 이름 변경
    나머지 테스트의 이름들도 xxxTotals로 변경
    
    Fred를 Customer로 변경하면 testMultipleRegularStatementFormat만
    실패한다.

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -8,11 +8,11 @@ public class VideoStoreTest {
 
     @Before
     public void setUp() {
-        statement = new Statement("Fred");
+        statement = new Statement("Customer");
     }
 
     @Test
-    public void testSingleNewReleaseStatement() {
+    public void testSingleNewReleaseStatementTotals() {
         statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         statement.generate();
         assertEquals(9.0d, statement.getTotal(), 0);
@@ -20,36 +20,39 @@ public class VideoStoreTest {
     }
 
     @Test
-    public void testDualNewReleaseStatement() {
+    public void testDualNewReleaseStatementTotals() {
         statement.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
         statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
-        assertEquals(
-                "Rental Record for Fred\n" +
-                        "\tThe Cell\t9.0\n" +
-                        "\tThe Tigger Movie\t9.0\n" +
-                        "Amount owed is 18.0\n" +
-                        "You earned 4 frequent renter points",
-                statement.generate());
+        statement.generate();
+        assertEquals(18.0, statement.getTotal(), 0);
+        assertEquals(4, statement.getFrequentRenterPoints());
     }
 
     @Test
-    public void testSingleChildrenStatement() {
+    public void testSingleChildrenStatementTotals() {
         statement.addRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
-        assertEquals(
-                "Rental Record for Fred\n" +
-                        "\tThe Tigger Movie\t1.5\n" +
-                        "Amount owed is 1.5\n" +
-                        "You earned 1 frequent renter points",
-                statement.generate());
+        statement.generate();
+        assertEquals(1.5, statement.getTotal(), 0);
+        assertEquals(1, statement.getFrequentRenterPoints());
     }
 
     @Test
-    public void testMultipleRegularStatement() {
+    public void testMultipleRegularStatementTotals() {
+        statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
+        statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
+        statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+        statement.generate();
+        assertEquals(7.5, statement.getTotal(), 0);
+        assertEquals(3, statement.getFrequentRenterPoints());
+    }
+
+    @Test
+    public void testMultipleRegularStatementFormat() {
         statement.addRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
         statement.addRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
         statement.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
         assertEquals(
-                "Rental Record for Fred\n" +
+                "Rental Record for Customer\n" +
                         "\tPlan 9 from Outer Space\t2.0\n" +
                         "\t8 1/2\t2.0\n" +
                         "\tEraserhead\t3.5\n" +
