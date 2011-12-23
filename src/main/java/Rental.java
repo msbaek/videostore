1c4b427aefa69a8b1ebd5b656a091b4f710781c4
Myeongseok Baek <msbaek@daumcorp.com>
2011-12-23 18:17:34 +0900

    Move Refactor - determineAmount
    이 함수는 Statement class의 field variable을 하나도 사용하지 않는다.
    Rental과 Movie의 field variable를 많이 사용한다.
    그러므로 이 함수는 Statement 클래스에 속하는 것이 적합하지 않다.
    함수의 인자로 Rental이 전달되므로 Movie와 Rental 중 Rental에 속하는 것이
    더 좋아보인다.
    
    ide의 Move Refatoring 조차 Rental을 추천한다.

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -22,4 +22,24 @@ public class Rental {
     public String getTitle() {
         return movie.getTitle();
     }
+
+    double determineAmount() {
+        double rentalAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (getDaysRented() > 2)
+                    rentalAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (getDaysRented() > 3)
+                    rentalAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
--- src/main/java/Statement.java
+++ src/main/java/Statement.java
@@ -46,31 +46,11 @@ public class Statement {
     }
 
     private String rentalLine(Rental rental) {
-        double rentalAmount = determineAmount(rental);
+        double rentalAmount = rental.determineAmount();
         frequentRenterPoints += determineFrequentRentalPoint(rental);
         totalAmount += rentalAmount;
 
         return formatRentalLine(rental, rentalAmount);
-    }
-
-    private double determineAmount(Rental rental) {
-        double rentalAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                rentalAmount += 2;
-                if (rental.getDaysRented() > 2)
-                    rentalAmount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                rentalAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                rentalAmount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    rentalAmount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
     }
 
     private int determineFrequentRentalPoint(Rental rental) {
