7af65d41a6dfbb91fc5590c969c8c3e626d9b57f
Myeongseok Baek <msbaek@daumcorp.com>
2011-12-23 18:39:37 +0900

    move determineAmount to Movie
    even my ide knows where to move

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -23,4 +23,24 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    double determineAmount(int daysRented, Rental rental) {
+        double rentalAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -20,27 +20,7 @@ public class Rental {
     }
 
     double determineAmount() {
-        return determineAmount(daysRented);
-    }
-
-    double determineAmount(int daysRented ) {
-        double rentalAmount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+        return movie.determineAmount(daysRented, this);
     }
 
     int determineFrequentRentalPoint() {
