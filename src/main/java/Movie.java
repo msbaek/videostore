9b9cb79301ca8f833ec847c48a45614e175401c1
Myeongseok Baek <msbaek71@gmail.com>
2011-12-24 10:47:34 +0900

    Push members down - determineAmount, determineFrequentRentalPoint
    Movie 클래스에서 determineAmount, determineFrequentRentalPoint 2개의
    메소드를 선택하여 push members down
    - keep abstract를 선택하여 base 클래스에는 abstract로 유지되도록
    - ChildrensMovie에서 priceCode가 private이여서 access가 안되므로 package
      local로 변경
    - test

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,11 +1,11 @@
-public class Movie {
+public abstract class Movie {
 
     public static final int CHILDRENS = 2;
     public static final int NEW_RELEASE = 1;
     public static final int REGULAR = 0;
 
     private String title;
-    private int pricecode;
+    int pricecode;
 
     public Movie(String title, int priceCode) {
         this.title = title;
@@ -24,30 +24,7 @@ public class Movie {
         return title;
     }
 
-    double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        switch (pricecode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
-    }
+    abstract double determineAmount(int daysRented);
 
-    int determineFrequentRentalPoint(int daysRented) {
-        boolean bonusIsEarned = (pricecode == NEW_RELEASE && daysRented > 1);
-        if (bonusIsEarned)
-            return 2;
-        return 1;
-    }
+    abstract int determineFrequentRentalPoint(int daysRented);
 }
--- src/main/java/NewReleaseMovie.java
+++ src/main/java/NewReleaseMovie.java
@@ -2,4 +2,31 @@ public class NewReleaseMovie extends Movie {
     public NewReleaseMovie(String title) {
         super(title, Movie.NEW_RELEASE);
     }
+
+    double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        switch (pricecode) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
+
+    int determineFrequentRentalPoint(int daysRented) {
+        boolean bonusIsEarned = (pricecode == NEW_RELEASE && daysRented > 1);
+        if (bonusIsEarned)
+            return 2;
+        return 1;
+    }
 }
