4aa33429f2edbfbcdb3aac14344aa6df07a15ac4
Myeongseok Baek <msbaek71@gmail.com>
2011-12-24 10:57:25 +0900

    remove unused codes - NewReleaseMovie
    조금씩 지우며, 중간 중간 테스트를 수행하면서...

--- src/main/java/videostore/NewReleaseMovie.java
+++ src/main/java/videostore/NewReleaseMovie.java
@@ -6,29 +6,10 @@ public class NewReleaseMovie extends Movie {
     }
 
     double determineAmount(int daysRented) {
-        double rentalAmount = 0;
-        switch (pricecode) {
-            case REGULAR:
-                rentalAmount += 2;
-                if (daysRented > 2)
-                    rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                rentalAmount += daysRented * 3;
-                break;
-            case CHILDRENS:
-                rentalAmount += 1.5;
-                if (daysRented > 3)
-                    rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+        return daysRented * 3;
     }
 
     int determineFrequentRentalPoint(int daysRented) {
-        boolean bonusIsEarned = (pricecode == NEW_RELEASE && daysRented > 1);
-        if (bonusIsEarned)
-            return 2;
-        return 1;
+        return (daysRented > 1) ? 2 : 1;
     }
 }
